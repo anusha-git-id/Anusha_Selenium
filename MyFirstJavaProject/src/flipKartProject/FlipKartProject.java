864be3e0b51166bbed7279410197b28e5e581f63
Anusha <Anusha@DESKTOP-8R65R88>
2020-02-26 19:31:56 +0530

    second commit

--- MyFirstJavaProject/src/flipKartProject/FlipKartProject.java
+++ MyFirstJavaProject/src/flipKartProject/FlipKartProject.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FlipKartProject {
 	
-	
+	//My first project
 	public static void main(String[] args) {
 		
 		WebDriver driver=new ChromeDriver();
